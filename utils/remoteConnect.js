5418cbd5506eec1a703256db8dd9931eaf478ad4
ikubiti <ikubiti@icloud.com>
2025-12-16 21:59:02 -0500

    updated npm packages

--- utils/remoteConnect.js
+++ utils/remoteConnect.js
@@ -9,6 +9,7 @@ const region = process.env.AWS_BUCKET_REGION || '';
 const accessKeyId = process.env.AWS_ACCESS_KEY || '';
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY || '';
 const awsUrl = process.env.AWS_CDN_URL || '';
+const saveLocation = process.env.AWS_PARENT_FOLDER || '';
 
 const s3Client = new S3Client({
   region,
@@ -19,15 +20,17 @@ const s3Client = new S3Client({
 });
 
 const uploadFile = async (fileBuffer, fileName, mimetype) => {
+  const fullFileName = saveLocation + fileName;
+
   const uploadParams = {
     Bucket: bucketName,
     Body: fileBuffer,
-    Key: fileName,
+    Key: fullFileName,
     ContentType: mimetype,
   };
 
   await s3Client.send(new PutObjectCommand(uploadParams));
-  return `${awsUrl}/${fileName}`;
+  return `${awsUrl}/${fullFileName}`;
 };
 
 const deleteFile = (fileName) => {
